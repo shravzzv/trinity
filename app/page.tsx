66bc6b31580ef4719431d31f90d8336317e26807
Sai Shravan <saishravan384@gmail.com>
2026-06-15 19:56:59 +0530

    chore: setup ShadCN

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
+import { Button } from '@/components/ui/button'
+
 export default function Home() {
   return (
     <div className='flex flex-1 flex-col items-center justify-center bg-zinc-50 font-sans dark:bg-black'>
       <p>Hello</p>
+      <Button>Hi</Button>
     </div>
   )
 }
