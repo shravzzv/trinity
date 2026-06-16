ed201ce0632c14d67373a0d6e8637aa86062e233
Sai Shravan <saishravan384@gmail.com>
2026-06-16 10:47:46 +0530

    refactor: use "it" instead of "test" in test blocks

--- __tests__/unit/app/~offline/page.test.tsx
+++ __tests__/unit/app/~offline/page.test.tsx
@@ -2,12 +2,12 @@ import Page from '@/app/~offline/page'
 import { render, screen } from '@testing-library/react'
 
 describe('Offline page', () => {
-  test(`should contain the offline message`, () => {
+  it(`should contain the offline message`, () => {
     render(<Page />)
     expect(screen.getByText('You are offline')).toBeInTheDocument()
   })
 
-  test('should match the snapshot', () => {
+  it('should match the snapshot', () => {
     const { container } = render(<Page />)
     expect(container).toMatchSnapshot()
   })
