fde9f34079e5ec7301bb0f0707546886edb5e476
Sai Shravan <saishravan384@gmail.com>
2026-06-16 10:15:54 +0530

    chore: allow dev origins from local network

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   reactCompiler: true,
+  allowedDevOrigins: ['192.168.0.104'],
 }
 
 export default withSerwist(nextConfig)
