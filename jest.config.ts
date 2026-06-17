17b07406424d1737659a35b6696f9b0d95dcdd02
Sai Shravan <saishravan384@gmail.com>
2026-06-17 10:18:02 +0530

    chore: update jest config with module name mrapper

--- jest.config.ts
+++ jest.config.ts
@@ -19,6 +19,9 @@ const config: Config = {
     '<rootDir>/components/ui',
     '<rootDir>/node_modules',
   ],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/$1',
+  },
 }
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
