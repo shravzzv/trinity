a6c009934de08e4ab2fb6aab5de71b8d35abc6d3
Sai Shravan <saishravan384@gmail.com>
2026-06-22 21:34:43 +0530

    feat: add time pickers UI for add fast dialog

--- components/add-fast-dialog.tsx
+++ components/add-fast-dialog.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import {
   Dialog,
   DialogClose,
@@ -9,11 +11,28 @@ import {
   DialogTrigger,
 } from '@/components/ui/dialog'
 import { Button } from './ui/button'
-import { Plus } from 'lucide-react'
+import { ChevronDownIcon, Plus } from 'lucide-react'
+import { useState } from 'react'
+import { Popover, PopoverContent, PopoverTrigger } from './ui/popover'
+import { Calendar } from './ui/calendar'
+import { format } from 'date-fns'
+import { Field, FieldGroup, FieldLabel } from './ui/field'
+import { Input } from './ui/input'
+import { Separator } from './ui/separator'
 
 export default function AddFastDialog() {
+  const [open, setOpen] = useState(false)
+  const [startDatePopoverOpen, setStartDatePopoverOpen] = useState(false)
+  const [endDatePopoverOpen, setEndDatePopoverOpen] = useState(false)
+  const [startDate, setStartDate] = useState<Date>()
+  const [endDate, setEndDate] = useState<Date>()
+
+  const handleContinue = () => {
+    setOpen(false)
+  }
+
   return (
-    <Dialog>
+    <Dialog open={open} onOpenChange={(open) => setOpen(open)}>
       <DialogTrigger asChild>
         <Button variant='secondary'>
           <Plus />
@@ -21,7 +40,7 @@ export default function AddFastDialog() {
         </Button>
       </DialogTrigger>
 
-      <DialogContent>
+      <DialogContent onInteractOutside={(e) => e.preventDefault()}>
         <DialogHeader>
           <DialogTitle>Add past fast</DialogTitle>
 
@@ -32,6 +51,104 @@ export default function AddFastDialog() {
           </DialogDescription>
         </DialogHeader>
 
+        <FieldGroup className='mx-auto flex-row'>
+          <Field>
+            <FieldLabel htmlFor='start-date-picker'>Start date</FieldLabel>
+            <Popover
+              open={startDatePopoverOpen}
+              onOpenChange={setStartDatePopoverOpen}
+            >
+              <PopoverTrigger asChild>
+                <Button
+                  variant='outline'
+                  id='start-date-picker'
+                  className='justify-between font-normal'
+                >
+                  {startDate ? format(startDate, 'PP') : 'Select date'}
+                  <ChevronDownIcon />
+                </Button>
+              </PopoverTrigger>
+
+              <PopoverContent
+                className='w-auto overflow-hidden p-0'
+                align='start'
+              >
+                <Calendar
+                  mode='single'
+                  selected={startDate}
+                  captionLayout='dropdown'
+                  defaultMonth={startDate}
+                  onSelect={(date) => {
+                    setStartDate(date)
+                    setStartDatePopoverOpen(false)
+                  }}
+                />
+              </PopoverContent>
+            </Popover>
+          </Field>
+
+          <Field>
+            <FieldLabel htmlFor='start-time-picker'>Time</FieldLabel>
+            <Input
+              id='start-time-picker'
+              type='time'
+              step='1'
+              defaultValue='18:00:00'
+              className='appearance-none pr-0 outline [&::-webkit-calendar-picker-indicator]:hidden [&::-webkit-calendar-picker-indicator]:appearance-none'
+            />
+          </Field>
+        </FieldGroup>
+
+        <Separator />
+
+        <FieldGroup className='mx-auto flex-row'>
+          <Field>
+            <FieldLabel htmlFor='end-date-picker'>End date</FieldLabel>
+            <Popover
+              open={endDatePopoverOpen}
+              onOpenChange={setEndDatePopoverOpen}
+            >
+              <PopoverTrigger asChild>
+                <Button
+                  variant='outline'
+                  id='end-date-picker'
+                  className='justify-between font-normal'
+                >
+                  {endDate ? format(endDate, 'PP') : 'Select date'}
+                  <ChevronDownIcon />
+                </Button>
+              </PopoverTrigger>
+
+              <PopoverContent
+                className='w-auto overflow-hidden p-0'
+                align='start'
+              >
+                <Calendar
+                  mode='single'
+                  selected={endDate}
+                  captionLayout='dropdown'
+                  defaultMonth={endDate}
+                  onSelect={(date) => {
+                    setEndDate(date)
+                    setEndDatePopoverOpen(false)
+                  }}
+                />
+              </PopoverContent>
+            </Popover>
+          </Field>
+
+          <Field>
+            <FieldLabel htmlFor='end-time-picker'>Time</FieldLabel>
+            <Input
+              id='end-time-picker'
+              type='time'
+              step='1'
+              defaultValue='17:30:00'
+              className='appearance-none pr-0 [&::-webkit-calendar-picker-indicator]:hidden [&::-webkit-calendar-picker-indicator]:appearance-none'
+            />
+          </Field>
+        </FieldGroup>
+
         <DialogFooter>
           <DialogClose asChild>
             <Button type='button' variant='outline'>
@@ -39,7 +156,7 @@ export default function AddFastDialog() {
             </Button>
           </DialogClose>
 
-          <Button>Continue</Button>
+          <Button onClick={handleContinue}>Continue</Button>
         </DialogFooter>
       </DialogContent>
     </Dialog>
