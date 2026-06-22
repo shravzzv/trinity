b0b92314cb8d48d59a567777877d65ab1efeded7
Sai Shravan <saishravan384@gmail.com>
2026-06-22 10:02:47 +0530

    chore: update allowed dev origins

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   reactCompiler: true,
-  allowedDevOrigins: ['192.168.0.104', '192.168.0.105'],
+  allowedDevOrigins: ['192.168.0.103', '192.168.0.104', '192.168.0.105'],
 }
 
 export default withSerwist(nextConfig)
