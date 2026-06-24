178f59d5493d224127ba98f2bb3f442683015742
Sai Shravan <saishravan384@gmail.com>
2026-06-24 08:46:51 +0530

    chore: update allowed dev origins

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,7 @@ const nextConfig: NextConfig = {
   reactCompiler: true,
   allowedDevOrigins: [
     '192.168.0.101',
+    '192.168.0.102',
     '192.168.0.103',
     '192.168.0.104',
     '192.168.0.105',
