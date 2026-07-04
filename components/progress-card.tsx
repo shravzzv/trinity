9efc3f121bbd924b9efce41f2392a8e7615e8ecf
Sai Shravan <saishravan384@gmail.com>
2026-07-04 21:39:25 +0530

    refactor: use Buttons for progress elements in the `ProgressCard`

--- components/progress-card.tsx
+++ components/progress-card.tsx
@@ -1,33 +1,37 @@
 import { Anchor, Flame, Trophy } from 'lucide-react'
 import { Card, CardContent } from './ui/card'
+import { Button } from './ui/button'
 
 export default function ProgressCard() {
   return (
     <Card>
-      <CardContent className='grid grid-cols-3 gap-2'>
-        <div className='bg-muted/40 flex flex-col items-center gap-1 rounded-lg border p-4'>
+      <CardContent className='grid grid-cols-3 gap-2 py-0'>
+        <Button variant='ghost' className='flex h-auto flex-col gap-1'>
           <div className='flex items-center gap-2'>
             <Flame className='size-5' />
             <p className='text-xl font-bold'>18</p>
           </div>
-          <p className='text-muted-foreground text-xs'>Streak</p>
-        </div>
 
-        <div className='bg-muted/40 flex flex-col items-center gap-1 rounded-lg border p-4'>
+          <p className='text-muted-foreground text-xs'>Streak</p>
+        </Button>
+
+        <Button variant='ghost' className='flex h-auto flex-col gap-1'>
           <div className='flex items-center gap-2'>
             <Anchor className='size-5' />
-            <p className='text-xl font-bold'>2</p>
+            <p className='text-xl font-bold'>12</p>
           </div>
-          <p className='text-muted-foreground text-xs'>Anchors</p>
-        </div>
 
-        <div className='bg-muted/40 flex flex-col items-center gap-1 rounded-lg border p-4'>
+          <p className='text-muted-foreground text-xs'>Anchors</p>
+        </Button>
+
+        <Button variant='ghost' className='flex h-auto flex-col gap-1'>
           <div className='flex items-center gap-2'>
             <Trophy className='size-5' />
             <p className='text-xl font-bold'>0</p>
           </div>
+
           <p className='text-muted-foreground text-xs'>Level</p>
-        </div>
+        </Button>
       </CardContent>
     </Card>
   )
