50bba5ec77cde9ff8c4c217f6edfe70e516eebcb
Sai Shravan <saishravan384@gmail.com>
2026-07-15 21:31:39 +0530

    feat: add menu nav site bar

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,7 +1,63 @@
 export default function Page() {
   return (
     <div>
-      <h1 className='text-3xl font-bold'>Trinity</h1>
+      <h1>Trinity</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni doloribus
+        facere impedit sunt dolorem mollitia delectus fugiat, tempora
+        laboriosam, necessitatibus maiores consequatur nihil quas hic dolores?
+        Recusandae enim vel, vitae laborum numquam voluptatem pariatur sequi, ad
+        officia delectus voluptatibus consectetur corrupti, eligendi cumque
+        aperiam ipsa rerum velit quisquam modi quae deserunt qui! Mollitia
+        consequatur sequi minus libero, molestias quaerat! Sed dignissimos quam
+        aut atque illo facilis, architecto placeat et optio porro accusantium
+        inventore in natus vel nulla, tempora, ullam temporibus! Quod vel illo
+        ab at quidem. Impedit officiis porro incidunt, molestiae necessitatibus
+        quod placeat, voluptas tempore quaerat pariatur totam eligendi quas
+        nobis mollitia. Omnis aut culpa est architecto cum ex eaque ad mollitia
+        accusamus sint animi provident, sed optio consequatur in possimus.
+        Dolores dolorem qui sint ratione. Quibusdam amet accusantium nisi
+        laboriosam fuga veniam ea reprehenderit, dolorum sunt, nihil asperiores.
+        Reiciendis dignissimos quaerat pariatur cum repellat quas dolor. Commodi
+        reprehenderit esse consequuntur quibusdam, recusandae corporis officia,
+        excepturi atque minima incidunt repudiandae inventore voluptatibus aut
+        nam facilis modi veniam sit delectus at, rerum architecto labore
+        voluptate explicabo pariatur. Distinctio tenetur fugiat animi nisi?
+        Ipsam laborum quaerat ex eius similique ut quisquam temporibus
+        praesentium mollitia quasi? Nam nemo quis facilis saepe amet! Esse quas
+        earum fugit at a, quidem vitae, adipisci eaque amet aperiam, harum
+        officiis optio perspiciatis. Tenetur numquam fugiat ipsam sint? Neque,
+        ab eum dignissimos sed quaerat consequuntur quo quibusdam ut aliquid,
+        unde voluptates vitae placeat voluptatum atque eligendi. Cumque
+        accusantium asperiores numquam amet, velit placeat dolorum. Quos sed
+        doloribus nihil cumque, eaque perferendis nostrum cum corporis natus
+        illum velit iure, accusantium ad mollitia debitis, neque adipisci ipsa
+        sit quae repudiandae nobis. Placeat enim et ex delectus eius rerum ab
+        qui, maxime asperiores laboriosam quas cumque! Nisi, soluta, cumque,
+        fugit assumenda rerum mollitia ducimus suscipit consequatur sed nostrum
+        magnam at harum sint incidunt sapiente labore error! Tempore similique
+        adipisci id, porro quod consequuntur quasi nostrum consequatur ipsam
+        cupiditate quae aspernatur accusantium nulla itaque nihil modi beatae
+        rem perferendis dicta est, voluptate quisquam dolorem veritatis. Nobis,
+        consequuntur officiis! Accusamus quam omnis tempore. Deleniti
+        consequuntur neque, distinctio blanditiis eligendi delectus dolor minus
+        praesentium iste ullam nisi iure id rem, repellendus nostrum hic. Culpa
+        ipsum eveniet nisi similique deserunt dicta itaque ipsa debitis, dolor
+        nulla illo quo reiciendis nemo tempora ut qui in quibusdam distinctio
+        placeat dolorum aliquam mollitia! Fugiat incidunt a quidem officiis
+        error quia esse assumenda sed temporibus excepturi, odit iure aliquam.
+        Tenetur molestias distinctio rerum hic optio omnis at corrupti
+        recusandae ad ratione, quia mollitia, dolor delectus repudiandae eaque
+        pariatur adipisci dolore qui, amet aspernatur impedit accusamus fugit
+        quisquam ipsum! Ducimus nisi corrupti nemo quo reprehenderit iure natus
+        totam facere exercitationem ipsum eaque odit, sapiente consequatur optio
+        repellendus. Sunt eius qui vel, perspiciatis suscipit rerum eaque eum at
+        exercitationem nulla cumque inventore tenetur optio harum porro minima
+        commodi quod, quis asperiores, nam fugiat accusantium eveniet. Error
+        officiis laudantium, aspernatur consectetur similique totam quia
+        blanditiis dicta maiores? Placeat natus pariatur, sint magni ex, neque
+        nemo, quas culpa provident enim mollitia soluta!
+      </p>
     </div>
   )
 }
--- components/site-navbar.tsx
+++ components/site-navbar.tsx
@@ -1,45 +1,51 @@
 'use client'
 
 import { siteLinks } from '@/constants/navigation'
-import { Menu } from 'lucide-react'
+import { cn } from '@/lib/utils'
+import { AnimatePresence, motion } from 'motion/react'
+import { Menu, X } from 'lucide-react'
 import Image from 'next/image'
 import Link from 'next/link'
-import { Button } from './ui/button'
 import { usePathname } from 'next/navigation'
-import { cn } from '@/lib/utils'
+import { useState } from 'react'
+import { Button } from './ui/button'
 
 export default function SiteNavbar() {
   const pathname = usePathname()
+  const [isMenuOpen, setIsMenuOpen] = useState(false)
+
+  const closeMenu = () => setIsMenuOpen(false)
 
   return (
-    <header className='bg-background/80 sticky top-0 backdrop-blur-xl'>
-      <nav className='mx-auto flex h-16 w-full max-w-6xl items-center px-6'>
-        <Link href='/' className='flex items-center gap-2'>
-          <Image
-            src='/icons/icon-512x512.png'
-            alt='Trinity'
-            width={32}
-            height={32}
-            className='rounded-lg'
-          />
+    <>
+      {/* Desktop navbar */}
+      <header className='bg-background/80 sticky top-0 z-50 hidden backdrop-blur-xl lg:block'>
+        <nav className='mx-auto flex h-16 max-w-6xl items-center px-6'>
+          <Link href='/' className='flex items-center gap-2'>
+            <Image
+              src='/icons/icon-512x512.png'
+              alt='Trinity'
+              width={32}
+              height={32}
+              className='rounded-lg'
+            />
 
-          <span className='text-base font-semibold'>Trinity</span>
-        </Link>
+            <span className='font-semibold'>Trinity</span>
+          </Link>
 
-        <div className='hidden flex-1 justify-center lg:flex'>
-          <div className='flex items-center gap-2'>
+          <div className='flex flex-1 justify-center gap-2'>
             {siteLinks.map((link) => {
               const isActive = pathname.startsWith(link.href)
 
               return (
                 <Button
-                  asChild
-                  size='lg'
-                  variant='link'
                   key={link.href}
+                  asChild
+                  variant='link'
+                  size='lg'
                   className={cn(
-                    isActive && 'underline',
                     'text-foreground font-normal',
+                    isActive && 'underline',
                   )}
                 >
                   <Link href={link.href}>{link.name}</Link>
@@ -47,32 +53,155 @@ export default function SiteNavbar() {
               )
             })}
           </div>
-        </div>
 
-        <div className='ml-auto flex items-center gap-2'>
-          <Button
-            asChild
-            size='lg'
-            variant='link'
-            className='text-foreground hidden font-normal lg:flex'
-          >
-            <Link href='/signin'>Sign in</Link>
-          </Button>
+          <div className='flex items-center gap-2'>
+            <Button
+              asChild
+              variant='link'
+              size='lg'
+              className='text-foreground font-normal'
+            >
+              <Link href='/signin'>Sign in</Link>
+            </Button>
 
-          <Button asChild>
-            <Link href='/home'>Get started</Link>
-          </Button>
+            <Button asChild>
+              <Link href='/home'>Get started</Link>
+            </Button>
+          </div>
+        </nav>
+      </header>
 
-          <Button
-            variant='ghost'
-            size='icon-lg'
-            className='lg:hidden'
-            aria-label='Open navigation menu'
-          >
-            <Menu className='size-5' />
-          </Button>
-        </div>
-      </nav>
-    </header>
+      {/* Mobile navigation */}
+      <AnimatePresence mode='wait'>
+        {!isMenuOpen ? (
+          <motion.header className='bg-background/80 sticky top-0 z-50 backdrop-blur-xl lg:hidden'>
+            <nav className='mx-auto flex h-16 items-center px-6'>
+              <Link href='/' className='flex items-center gap-2'>
+                <Image
+                  src='/icons/icon-512x512.png'
+                  alt='Trinity'
+                  width={32}
+                  height={32}
+                  className='rounded-lg'
+                />
+
+                <span className='font-semibold'>Trinity</span>
+              </Link>
+
+              <div className='ml-auto flex items-center gap-2'>
+                <Button asChild>
+                  <Link href='/home'>Get started</Link>
+                </Button>
+
+                <Button
+                  variant='ghost'
+                  size='icon-lg'
+                  aria-label='Open navigation menu'
+                  onClick={() => setIsMenuOpen(true)}
+                >
+                  <Menu className='size-5' />
+                </Button>
+              </div>
+            </nav>
+          </motion.header>
+        ) : (
+          <motion.div className='bg-background fixed inset-0 z-50 flex flex-col lg:hidden'>
+            {/* Header */}
+            <header>
+              <nav className='mx-auto flex h-16 items-center px-6'>
+                <Link
+                  href='/'
+                  onClick={closeMenu}
+                  className='flex items-center gap-2'
+                >
+                  <Image
+                    src='/icons/icon-512x512.png'
+                    alt='Trinity'
+                    width={32}
+                    height={32}
+                    className='rounded-lg'
+                  />
+
+                  <span className='font-semibold'>Trinity</span>
+                </Link>
+
+                <div className='ml-auto flex items-center gap-2'>
+                  <Button asChild>
+                    <Link href='/home' onClick={closeMenu}>
+                      Get started
+                    </Link>
+                  </Button>
+
+                  <Button
+                    size='icon-lg'
+                    variant='ghost'
+                    onClick={closeMenu}
+                    aria-label='Close navigation menu'
+                  >
+                    <X className='size-5' />
+                  </Button>
+                </div>
+              </nav>
+            </header>
+
+            <motion.div
+              initial='hidden'
+              animate='visible'
+              variants={{
+                hidden: {},
+                visible: {
+                  transition: { staggerChildren: 0.05 },
+                },
+              }}
+              className='flex flex-1 flex-col px-6 py-8'
+            >
+              {siteLinks.map((link) => {
+                const isActive = pathname.startsWith(link.href)
+
+                return (
+                  <motion.div
+                    key={link.href}
+                    variants={{
+                      hidden: {
+                        opacity: 0,
+                        y: 12,
+                      },
+                      visible: {
+                        opacity: 1,
+                        y: 0,
+                      },
+                    }}
+                  >
+                    <Button
+                      asChild
+                      size='lg'
+                      variant={isActive ? 'secondary' : 'ghost'}
+                      className='mb-1 w-full justify-start text-lg'
+                    >
+                      <Link href={link.href} onClick={closeMenu}>
+                        {link.name}
+                      </Link>
+                    </Button>
+                  </motion.div>
+                )
+              })}
+
+              <div className='mt-6 border-t pt-6'>
+                <Button
+                  asChild
+                  size='lg'
+                  variant='ghost'
+                  className='w-full justify-start text-lg'
+                >
+                  <Link href='/signin' onClick={closeMenu}>
+                    Sign in
+                  </Link>
+                </Button>
+              </div>
+            </motion.div>
+          </motion.div>
+        )}
+      </AnimatePresence>
+    </>
   )
 }
