c6a8bf48383953f885d5fb62b674d3f241f5b5e8
Sai Shravan <saishravan384@gmail.com>
2026-07-15 11:28:47 +0530

    feat: introduce context providers

--- app/(app)/layout.tsx
+++ app/(app)/layout.tsx
@@ -1,6 +1,9 @@
 import AppBottomNav from '@/components/app-bottom-nav'
 import { AppSidebar } from '@/components/app-sidebar'
 import { SidebarProvider } from '@/components/ui/sidebar'
+import { FastingProvider } from '@/providers/fasting-provider'
+import { GamificationProvider } from '@/providers/gamification-provider'
+import { WeightProvider } from '@/providers/weight-provider'
 import { cookies } from 'next/headers'
 
 export default async function Layout({
@@ -12,10 +15,16 @@ export default async function Layout({
   const defaultOpen = cookieStore.get('sidebar_state')?.value === 'true'
 
   return (
-    <SidebarProvider defaultOpen={defaultOpen}>
-      <AppSidebar />
-      <main className='mx-auto w-full max-w-xl p-6'>{children}</main>
-      <AppBottomNav />
-    </SidebarProvider>
+    <FastingProvider>
+      <WeightProvider>
+        <GamificationProvider>
+          <SidebarProvider defaultOpen={defaultOpen}>
+            <AppSidebar />
+            <main className='mx-auto w-full max-w-xl p-6'>{children}</main>
+            <AppBottomNav />
+          </SidebarProvider>
+        </GamificationProvider>
+      </WeightProvider>
+    </FastingProvider>
   )
 }
