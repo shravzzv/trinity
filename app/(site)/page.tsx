54943ffba204998cc856c896eba9e1b748ae8e09
Sai Shravan <saishravan384@gmail.com>
2026-07-18 21:20:32 +0530

    feat: add hero section of the landing page

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,63 +1,61 @@
+'use client'
+
+import { Button } from '@/components/ui/button'
+import { motion } from 'motion/react'
+import Link from 'next/link'
+
 export default function Page() {
   return (
     <div>
-      <h1>Trinity</h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni doloribus
-        facere impedit sunt dolorem mollitia delectus fugiat, tempora
-        laboriosam, necessitatibus maiores consequatur nihil quas hic dolores?
-        Recusandae enim vel, vitae laborum numquam voluptatem pariatur sequi, ad
-        officia delectus voluptatibus consectetur corrupti, eligendi cumque
-        aperiam ipsa rerum velit quisquam modi quae deserunt qui! Mollitia
-        consequatur sequi minus libero, molestias quaerat! Sed dignissimos quam
-        aut atque illo facilis, architecto placeat et optio porro accusantium
-        inventore in natus vel nulla, tempora, ullam temporibus! Quod vel illo
-        ab at quidem. Impedit officiis porro incidunt, molestiae necessitatibus
-        quod placeat, voluptas tempore quaerat pariatur totam eligendi quas
-        nobis mollitia. Omnis aut culpa est architecto cum ex eaque ad mollitia
-        accusamus sint animi provident, sed optio consequatur in possimus.
-        Dolores dolorem qui sint ratione. Quibusdam amet accusantium nisi
-        laboriosam fuga veniam ea reprehenderit, dolorum sunt, nihil asperiores.
-        Reiciendis dignissimos quaerat pariatur cum repellat quas dolor. Commodi
-        reprehenderit esse consequuntur quibusdam, recusandae corporis officia,
-        excepturi atque minima incidunt repudiandae inventore voluptatibus aut
-        nam facilis modi veniam sit delectus at, rerum architecto labore
-        voluptate explicabo pariatur. Distinctio tenetur fugiat animi nisi?
-        Ipsam laborum quaerat ex eius similique ut quisquam temporibus
-        praesentium mollitia quasi? Nam nemo quis facilis saepe amet! Esse quas
-        earum fugit at a, quidem vitae, adipisci eaque amet aperiam, harum
-        officiis optio perspiciatis. Tenetur numquam fugiat ipsam sint? Neque,
-        ab eum dignissimos sed quaerat consequuntur quo quibusdam ut aliquid,
-        unde voluptates vitae placeat voluptatum atque eligendi. Cumque
-        accusantium asperiores numquam amet, velit placeat dolorum. Quos sed
-        doloribus nihil cumque, eaque perferendis nostrum cum corporis natus
-        illum velit iure, accusantium ad mollitia debitis, neque adipisci ipsa
-        sit quae repudiandae nobis. Placeat enim et ex delectus eius rerum ab
-        qui, maxime asperiores laboriosam quas cumque! Nisi, soluta, cumque,
-        fugit assumenda rerum mollitia ducimus suscipit consequatur sed nostrum
-        magnam at harum sint incidunt sapiente labore error! Tempore similique
-        adipisci id, porro quod consequuntur quasi nostrum consequatur ipsam
-        cupiditate quae aspernatur accusantium nulla itaque nihil modi beatae
-        rem perferendis dicta est, voluptate quisquam dolorem veritatis. Nobis,
-        consequuntur officiis! Accusamus quam omnis tempore. Deleniti
-        consequuntur neque, distinctio blanditiis eligendi delectus dolor minus
-        praesentium iste ullam nisi iure id rem, repellendus nostrum hic. Culpa
-        ipsum eveniet nisi similique deserunt dicta itaque ipsa debitis, dolor
-        nulla illo quo reiciendis nemo tempora ut qui in quibusdam distinctio
-        placeat dolorum aliquam mollitia! Fugiat incidunt a quidem officiis
-        error quia esse assumenda sed temporibus excepturi, odit iure aliquam.
-        Tenetur molestias distinctio rerum hic optio omnis at corrupti
-        recusandae ad ratione, quia mollitia, dolor delectus repudiandae eaque
-        pariatur adipisci dolore qui, amet aspernatur impedit accusamus fugit
-        quisquam ipsum! Ducimus nisi corrupti nemo quo reprehenderit iure natus
-        totam facere exercitationem ipsum eaque odit, sapiente consequatur optio
-        repellendus. Sunt eius qui vel, perspiciatis suscipit rerum eaque eum at
-        exercitationem nulla cumque inventore tenetur optio harum porro minima
-        commodi quod, quis asperiores, nam fugiat accusantium eveniet. Error
-        officiis laudantium, aspernatur consectetur similique totam quia
-        blanditiis dicta maiores? Placeat natus pariatur, sint magni ex, neque
-        nemo, quas culpa provident enim mollitia soluta!
-      </p>
+      <div className='flex flex-col items-center gap-6 py-12 text-center md:py-20'>
+        <div className='leading-2'>
+          <motion.h1
+            className='text-5xl leading-tight font-bold tracking-tight sm:text-6xl md:text-7xl lg:text-8xl'
+            initial={{ opacity: 0, y: 16 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{
+              duration: 0.5,
+              ease: 'easeOut',
+            }}
+          >
+            Track your fasts
+            <span className='block'>
+              <motion.span
+                className='inline-block bg-linear-to-r from-violet-500 via-sky-500 to-violet-500 bg-size-[200%_100%] bg-clip-text text-transparent'
+                animate={{
+                  backgroundPosition: ['0% 50%', '100% 50%', '0% 50%'],
+                }}
+                transition={{
+                  duration: 8,
+                  repeat: Infinity,
+                  ease: 'linear',
+                }}
+              >
+                beautifully
+              </motion.span>
+            </span>
+          </motion.h1>
+        </div>
+
+        <motion.p
+          initial={{ opacity: 0, y: 8 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ delay: 0.15 }}
+        >
+          A clean, offline-first intermittent fasting tracker designed to help
+          you stay consistent.
+        </motion.p>
+
+        <motion.div
+          initial={{ opacity: 0, y: 8 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ delay: 0.3 }}
+        >
+          <Button asChild size='lg'>
+            <Link href='/home'>Get started</Link>
+          </Button>
+        </motion.div>
+      </div>
     </div>
   )
 }
