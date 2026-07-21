c446ca62f97678b9ddb7bd4a2d8c48783686fd7b
Sai Shravan <saishravan384@gmail.com>
2026-07-21 21:24:25 +0530

    feat: add a docs file to constants to act as source of links

---
+++ constants/docs.ts
@@ -0,0 +1,38 @@
+export const docs = [
+  {
+    title: 'Getting Started',
+    href: '/docs/getting-started',
+  },
+  {
+    title: 'Timer',
+    href: '/docs/timer',
+  },
+  {
+    title: 'Plans',
+    href: '/docs/plans',
+  },
+  {
+    title: 'Statistics',
+    href: '/docs/statistics',
+  },
+  {
+    title: 'Weight',
+    href: '/docs/weight',
+  },
+  {
+    title: 'Gamification',
+    href: '/docs/gamification',
+  },
+  {
+    title: 'Anchors',
+    href: '/docs/anchors',
+  },
+  {
+    title: 'FAQ',
+    href: '/docs/faq',
+  },
+  {
+    title: 'Changelog',
+    href: '/docs/changelog',
+  },
+]
