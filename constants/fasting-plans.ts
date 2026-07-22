37230bd8044854e061ed443884d33cc7feecc855
Sai Shravan <saishravan384@gmail.com>
2026-07-22 15:52:32 +0530

    feat: add more fasting plans

--- constants/fasting-plans.ts
+++ constants/fasting-plans.ts
@@ -2,6 +2,18 @@ import type { FastingPlan } from '@/types/fasting'
 
 export const fastingPlans: FastingPlan[] = [
   {
+    id: '12:12',
+    title: '12:12',
+    fastingHours: 12,
+    eatingHours: 12,
+  },
+  {
+    id: '14:10',
+    title: '14:10',
+    fastingHours: 14,
+    eatingHours: 10,
+  },
+  {
     id: '16:8',
     title: '16:8',
     fastingHours: 16,
