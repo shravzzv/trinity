17dde16920f6f29325f1456feb9a6b724b9d580f
Sai Shravan <saishravan384@gmail.com>
2026-07-24 20:23:40 +0530

    feat: add support page

--- constants/links.ts
+++ constants/links.ts
@@ -1,3 +1,5 @@
 export const DISCORD_INVITE = 'https://discord.gg/UakeUbdwkb'
 
 export const GITHUB_REPOSITORY_LINK = 'https://github.com/shravzzv/trinity'
+
+export const SUPPORT_EMAIL = 'shravzzv@outlook.com'
