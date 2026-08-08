78ee4180d156533dcbf904b6875e906395b88d79
Sai Shravan <saishravan384@gmail.com>
2026-08-08 15:05:33 +0530

    feat: add realtime provider

--- app/(app)/layout.tsx
+++ app/(app)/layout.tsx
@@ -3,6 +3,7 @@ import { AppSidebar } from '@/components/app-sidebar'
 import { SidebarProvider } from '@/components/ui/sidebar'
 import { FastingProvider } from '@/providers/fasting-provider'
 import { GamificationProvider } from '@/providers/gamification-provider'
+import { RealtimeProvider } from '@/providers/realtime-provider'
 import { WeightProvider } from '@/providers/weight-provider'
 import { cookies } from 'next/headers'
 
@@ -15,16 +16,18 @@ export default async function Layout({
   const defaultOpen = cookieStore.get('sidebar_state')?.value === 'true'
 
   return (
-    <FastingProvider>
-      <WeightProvider>
-        <GamificationProvider>
-          <SidebarProvider defaultOpen={defaultOpen}>
-            <AppSidebar />
-            <main className='w-full p-6 pb-24 lg:pb-6'>{children}</main>
-            <AppBottomNav />
-          </SidebarProvider>
-        </GamificationProvider>
-      </WeightProvider>
-    </FastingProvider>
+    <RealtimeProvider>
+      <FastingProvider>
+        <WeightProvider>
+          <GamificationProvider>
+            <SidebarProvider defaultOpen={defaultOpen}>
+              <AppSidebar />
+              <main className='w-full p-6 pb-24 lg:pb-6'>{children}</main>
+              <AppBottomNav />
+            </SidebarProvider>
+          </GamificationProvider>
+        </WeightProvider>
+      </FastingProvider>
+    </RealtimeProvider>
   )
 }
