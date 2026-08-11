b0606e8f58f122c6301a120a59a062c7f6353a48
Sai Shravan <saishravan384@gmail.com>
2026-08-11 19:59:16 +0530

    feat: add skeleton for settings account auth card when loading

--- components/settings-account-auth-card.tsx
+++ components/settings-account-auth-card.tsx
@@ -20,9 +20,27 @@ import {
   AlertDialogTrigger,
 } from './ui/alert-dialog'
 import { toast } from 'sonner'
+import { Skeleton } from './ui/skeleton'
 
 export default function SettingsAccountAuthCard() {
-  const { isAuthenticated } = useAuthContext()
+  const { isAuthenticated, isLoading } = useAuthContext()
+
+  if (isLoading) {
+    return (
+      <Card>
+        <CardContent>
+          <div className='flex items-center justify-between'>
+            <div className='space-y-1'>
+              <Skeleton className='h-6 w-32' />
+              <Skeleton className='h-4 w-40 md:w-64' />
+              <Skeleton className='h-4 w-40 md:w-64' />
+            </div>
+            <Skeleton className='h-8 w-24' />
+          </div>
+        </CardContent>
+      </Card>
+    )
+  }
 
   return (
     <Card>
